a077de63f1a55941747c07a1868bf27187dc5fb7
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-03-13 03:31:12 +0200

    Chore: Add page structure and sass setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,15 @@
 
 export default function Home() {
   return (
-    <h1>Portfolio</h1>
+    <div>
+      <section>Hero</section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallax </section>
+      <section>Portfolio Project 1</section>
+      <section>Portfolio Project 2</section>
+      <section>Portfolio Project 3</section>
+      <section>Contact</section>
+    </div>
   );
 }
