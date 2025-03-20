b31a47e6ef71ac9b65e4aafa7e932d0965000bd6
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-03-20 05:33:09 +0200

    Feat: Add sidebar component

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -1,13 +1,25 @@
+"use client";
+import { motion } from "framer-motion";
 import React from "react";
 import "./Navbar.scss";
 import { FaLinkedin, FaXTwitter, FaYoutube } from "react-icons/fa6";
 import { FaFacebookF } from "react-icons/fa";
+import Sidebar from "../Sidebar/Sidebar";
 
 const Navbar = () => {
   return (
     <div className="navbar">
+      {/* Sidebar */}
+      <Sidebar />
       <div className="wrapper">
-        <p className="logo">Rawda Yasser</p>
+        <motion.p
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="logo"
+        >
+          Rawda Yasser
+        </motion.p>
         <div className="social-links">
           <a href="#">
             <FaFacebookF className="social-icon" />
--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,16 @@ import Navbar from "./components/Navbar/Navbar";
 export default function Home() {
   return (
     <div>
-      <section><Navbar /></section>
-      <section>Parallax</section>
+      <section id="Homepage">
+        <Navbar />
+      </section>
+      <section id="Services">Parallax</section>
       <section>Services</section>
-      <section>Parallax </section>
+      <section id="Portfolio"> Parallax </section>
       <section>Portfolio Project 1</section>
       <section>Portfolio Project 2</section>
       <section>Portfolio Project 3</section>
-      <section>Contact</section>
+      <section id="Contact">Contact</section>
     </div>
   );
 }
